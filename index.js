ddc9a31ce013f9bb96475b6c3ef4e0a08be99b2b
John Genere <johncgenere@gmail.com>
2019-05-31 12:18:41 -0400

    Made change all color and reset all functional

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 function start(){
   document.getElementById('table').style.display = 'flex';
-  document.getElementsByClassName('myButton')[0].style.display = 'none';
+  // document.getElementsByClassName('myButton')[0].style.display = 'none';
+  document.getElementById('start').style.display = 'none';
+}
+
+function addRow(){
+
+}
+
+function removeRow(){
+
+}
+
+function addCol(){
+
+}
+
+function removeCol(){
+
+}
+
+function getColor(){
+  var selectedColor = document.getElementById("colors").selectedIndex;
+  var allOptions = document.getElementById("colors").options;
+  return allOptions[selectedColor].text;
+  // document.getElementById('cell').style.backgroundColor = "black";
+}
+
+function setAll(){
+  var allCells = document.getElementsByTagName('td');
+  for (var i = 0; i < allCells.length; i++) {
+    allCells[i].style.backgroundColor = getColor();
+  }
+}
+
+function resetAll(){
+  var allCells = document.getElementsByTagName('td');
+  for (var i = 0; i < allCells.length; i++) {
+    allCells[i].style.backgroundColor = "white";
+  }
 }
