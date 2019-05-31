90a5220c716e8037eaae4bb1d7916e95c01391cc
John Genere <johncgenere@gmail.com>
2019-05-31 14:39:08 -0400

    Add row is now functional

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 function start(){
   document.getElementById('table').style.display = 'flex';
-  // document.getElementsByClassName('myButton')[0].style.display = 'none';
   document.getElementById('start').style.display = 'none';
 }
 
 function addRow(){
-
+  let allRows = document.getElementById('table');
+  let getRow = allRows.childNodes[1].children[0];
+  let cloned = getRow.cloneNode(true);
+  allRows.childNodes[1].appendChild(cloned);
 }
 
-function removeRow(){
+function removeRow(btn){
+  console.log('clicked removerow');
 
 }
 
 function addCol(){
-
+  let allRows = document.getElementsByTagName('tr');
+  let newRow = document.getElementById('cell');
+  let cloned = newRow.cloneNode(true);
+  for (let i = 0; i < allRows.length;i++) {
+    console.log(allRows[i]);
+    allRows[i].appendChild(cloned);
+  }
 }
 
 function removeCol(){
-
+  console.log('clicked removecol');
 }
 
 function getColor(){
-  var selectedColor = document.getElementById("colors").selectedIndex;
-  var allOptions = document.getElementById("colors").options;
+  let selectedColor = document.getElementById("colors").selectedIndex;
+  let allOptions = document.getElementById("colors").options;
   return allOptions[selectedColor].text;
-  // document.getElementById('cell').style.backgroundColor = "black";
 }
 
 function setAll(){
-  var allCells = document.getElementsByTagName('td');
-  for (var i = 0; i < allCells.length; i++) {
+  let allCells = document.getElementsByTagName('td');
+  for (let i = 0; i < allCells.length; i++) {
     allCells[i].style.backgroundColor = getColor();
   }
 }
 
 function resetAll(){
-  var allCells = document.getElementsByTagName('td');
-  for (var i = 0; i < allCells.length; i++) {
+  let allCells = document.getElementsByTagName('td');
+  for (let i = 0; i < allCells.length; i++) {
     allCells[i].style.backgroundColor = "white";
   }
 }
